9b5cb709e4106fa61951a2dc51273a17ed13bf78
Salman-18 <salmannizamani205@gmail.com>
2024-02-21 20:56:57 +0500

    pasword encrypted successfully

--- password_manager.py
+++ password_manager.py
@@ -1,49 +1,68 @@
 from cryptography.fernet import Fernet
+import os
 
 
+# Function to generate a key if it doesn't exist
+def generate_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+
+
+# Function to load the key from file or generate if not exist
 def load_key():
-    file = open("key.key", "rb")
-    key = file.read()
-    file.close()
+    if not os.path.exists("key.key"):
+        generate_key()
+    with open("key.key", "rb") as file:
+        key = file.read()
     return key
 
 
-master_pwd = input("What is the master password?: ")
-key = load_key() + master_pwd.encode()
-fer = Fernet(key)
+# Load the key
+key = load_key()
+fer = Fernet(key)  # Instantiate Fernet with the loaded key
 
 
+# Function to view passwords
 def view():
-    with open("passwords.txt", "a") as f:
-        for line in f.readlines():
-            data = line.rstrip()
-            user, passw = data.split("|")
-            print("user: ", user, "| passwords: ", fer.decrypt(passw.encode()).decode())
+    try:
+        with open("passwords.txt", "r") as f:
+            lines = f.readlines()
+            if not lines:
+                print("No passwords stored yet.")
+                return
+            for line in lines:
+                data = line.rstrip()
+                user, passw = data.split("|")
+                print(
+                    "user:", user, "| password:", fer.decrypt(passw.encode()).decode()
+                )
+    except FileNotFoundError:
+        print("Password file not found.")
 
 
+# Function to add passwords
 def add():
     name = input("Account Name: ")
-    pwd = input("password: ")
+    pwd = input("Password: ")
+    encrypted_pwd = fer.encrypt(pwd.encode()).decode()
     with open("passwords.txt", "a") as f:
-        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+        f.write(name + "|" + encrypted_pwd + "\n")
 
 
+# Main loop
 while True:
     mode = input(
-        "would you like to add a new password or view existing ones (view, add) press q to quit: "
+        "Would you like to add a new password or view existing ones (view, add) Press 'q' to quit: "
     ).lower()
     if mode == "q":
         break
-    if mode == "view":
-
+    elif mode == "view":
         view()
-
     elif mode == "add":
-
         add()
     else:
-        print("invaild mode. ")
-        continue
+        print("Invalid mode.")
 
     """def write_key():
     key = Fernet.generate_key()
